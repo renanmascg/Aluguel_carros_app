a2278924bcfe325e8d43ce729f9fde8652c8af79
renanmascg <renanmascg@gmail.com>
2022-02-28 22:43:38 -0300

    Refatora o código dentro da aplicação, agora organizado em modules permitindo o desenvolvimento melhor. Implementa, também, a parte de cadastro de especificações dentro da aplicação

---
+++ src/routes/Specifications.routes.ts
@@ -0,0 +1,21 @@
+import { Router } from "express";
+
+import { SpecificationsRepository } from "../modules/cars/repositories/SpecificationsRepository";
+import { CreateSpecificationService } from "../modules/cars/services/CreateSpecificationService";
+
+const specificationsRoutes = Router();
+const specificationRepository = new SpecificationsRepository();
+
+specificationsRoutes.post("/", (request, response) => {
+  const { name, description } = request.body;
+
+  const specificationService = new CreateSpecificationService(
+    specificationRepository
+  );
+
+  specificationService.execute({ name, description });
+
+  return response.status(201).send();
+});
+
+export { specificationsRoutes };
