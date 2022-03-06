dfa79b593823e3a45293518b5e89d14450b77ec5
renanmascg <renanmascg@gmail.com>
2022-03-06 20:33:01 -0300

    Adiciona criptografia na senha e validação para evitar duplicidades de email.

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,4 @@
+import { hash } from "bcryptjs";
 import { inject, injectable } from "tsyringe";
 
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
@@ -16,11 +17,19 @@ class CreateUserUseCase {
     driver_license,
     password,
   }: ICreateUserDTO): Promise<void> {
+    const passwordHash = await hash(password, 8);
+
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
     await this.usersRepository.create({
       name,
       email,
       driver_license,
-      password,
+      password: passwordHash,
     });
   }
 }
