9c3ad9b412f6dca00560555d80ef3278949a97c5
renanmascg <renanmascg@gmail.com>
2022-03-22 12:35:56 -0300

    Adiciona criação de usuário real no banco de dados, implementando os containers e todos os repositorios

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
 import { authenticateRoutes } from "./Authenticate.routes";
+import { carsRoutes } from "./Cars.routes";
 import { categoriesRoutes } from "./Categories.routes";
 import { specificationsRoutes } from "./Specifications.routes";
 import { usersRoutes } from "./Users.routes";
@@ -10,6 +11,7 @@ const router = Router();
 router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
 router.use("/users", usersRoutes);
+router.use("/cars", carsRoutes);
 router.use(authenticateRoutes);
 
 export { router };
