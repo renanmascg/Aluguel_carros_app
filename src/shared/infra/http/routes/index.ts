04d395d390a8663407936c940529dd18838597a0
renanmascg <renanmascg@gmail.com>
2022-04-19 17:23:25 -0300

    Termina a implementação com conexão real ao banco de dados dos alugueis de carros ( rentals )

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { authenticateRoutes } from "./Authenticate.routes";
 import { carsRoutes } from "./Cars.routes";
 import { categoriesRoutes } from "./Categories.routes";
+import { rentalRoutes } from "./Rental.routes";
 import { specificationsRoutes } from "./Specifications.routes";
 import { usersRoutes } from "./Users.routes";
 
@@ -12,6 +13,7 @@ router.use("/categories", categoriesRoutes);
 router.use("/specifications", specificationsRoutes);
 router.use("/users", usersRoutes);
 router.use("/cars", carsRoutes);
+router.use("/rentals", rentalRoutes);
 router.use(authenticateRoutes);
 
 export { router };
