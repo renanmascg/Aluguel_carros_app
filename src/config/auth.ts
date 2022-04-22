3b02c0d1932125635f701b83fb357ce714ad3a0d
renanmascg <renanmascg@gmail.com>
2022-04-22 16:02:00 -0300

    Adiciona o refresh token na saida do login e refatora a parte de autenticação do códigocom o auth.ts

---
+++ src/config/auth.ts
@@ -0,0 +1,7 @@
+export default {
+  secret_token: "c345cbd944a1473d22d61895512133c6",
+  expires_in_token: "15m",
+  secret_refresh_token: "6ae02b21804f71c25d8361c80c6ac81c",
+  expires_in_refresh_token: "30d",
+  expires_refresh_token_days: 30,
+};
