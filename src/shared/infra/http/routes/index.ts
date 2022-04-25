e71d66c41b65a551d16a02b98c668f61b7948ccf
renanmascg <renanmascg@gmail.com>
2022-04-25 19:31:55 -0300

    Implementa o Provider de enviar email dentro da aplicação.

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { authenticateRoutes } from "./Authenticate.routes";
 import { carsRoutes } from "./Cars.routes";
 import { categoriesRoutes } from "./Categories.routes";
+import { passwordRoutes } from "./password.routes";
 import { rentalRoutes } from "./Rental.routes";
 import { specificationsRoutes } from "./Specifications.routes";
 import { usersRoutes } from "./Users.routes";
@@ -14,6 +15,7 @@ router.use("/specifications", specificationsRoutes);
 router.use("/users", usersRoutes);
 router.use("/cars", carsRoutes);
 router.use("/rentals", rentalRoutes);
+router.use("/password", passwordRoutes);
 router.use(authenticateRoutes);
 
 export { router };
